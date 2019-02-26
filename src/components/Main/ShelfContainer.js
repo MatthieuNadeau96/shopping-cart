dd68ce236c92de0084e9de02f87e90fd1446af8b
Matthieu <matthieunadeau96@gmail.com>
2019-02-26 13:22:20 -0500

    fixed the display for the shelfItems

--- src/components/Main/ShelfContainer.js
+++ src/components/Main/ShelfContainer.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 
-import ShelfItem from './ShelfItem';
 
 class ShelfContainer extends Component {
 
   render() {
+    let items = [];
+    for(var i = 0; i < 16; i++) {
+      items.push(
+        <div className="shelf-item">
+          Item{[i]}
+        </div>
+      )
+    }
     return (
       <div className="shelfContainer">
-        <h2>Shelf Container</h2>
-        <ShelfItem/>
+        <div className="shelfHeaderContainer">Shelf Header</div>
+        <div className="shelfItemsContainer">
+          {items}
+        </div>
       </div>
     );
   }
